3c731c73f1cb86e277dc082d9661f65977d13bb5
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-05-07 09:13:34 +0000

    Modified allowed host as project would not run on startup. base.html layout modified

--- ceb_chalk_art/settings.py
+++ ceb_chalk_art/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eti9un!3dhm9vm4+&m(%bi#ftq1$%n2608g7)e7%o(9*zbrfjg'
+SECRET_KEY = '-5=(ya)l)##gwv$yzpny#6zk%ddah1%%vpzk(95^3i_%eu$4#^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-coder4916-cebchalkart-xmxqv4w7czo.ws-eu111.gitpod.io'
+    '8000-coder4916-cebchalkart-xmxqv4w7czo.ws-eu113.gitpod.io'
 ]
 
 
