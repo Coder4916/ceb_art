2fb827a8c3b1899ba88b4ec9450da81a26c659e4
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-05-09 18:10:45 +0000

    DEBUG and SECRET key imported from env.py

--- ceb_chalk_art/settings.py
+++ ceb_chalk_art/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = [
     '8000-coder4916-cebchalkart-xmxqv4w7czo.ws-eu111.gitpod.io'
