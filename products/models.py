5d387d0be2312cc852a8cf0e236c6707782fca53
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-05-13 14:26:52 +0000

    Added more products to db and updated admin display in the products app

--- products/models.py
+++ products/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
     name = models.TextField()
     friendly_name = models.TextField(null = True, blank=True)
 
@@ -13,13 +16,13 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.TextField(null=True, blank=True)
-    name = models.TextField()
+    art_category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    product_num = models.TextField(null=True, blank=True)
+    artwork = models.TextField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    art_image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.artwork
