bed2c36e65e095ace22a8d7fe867b3245248ebd1
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-05-14 10:01:52 +0000

    Created and connected up a products.html page to the site and have begun adding products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def art_products(request):
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
