8c1661b5b8f9dfd617b450e882007d4c7794741f
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-05-15 21:38:31 +0000

    Created a details page for each piece of artwork using Bootstrap themes, and linked in views/urls

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.art_products, name='products')
+    path('', views.art_products, name='products'),
+    path('<product_id>', views.artwork_details, name='artwork_details'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
-# Create your views here.
 
+#A view to render the art_products
 def art_products(request):
+
     products = Product.objects.all()
+
     context = {
         'products': products,
     }
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/art_products.html', context)
+
+
+#A view to render the artwork details
+def artwork_details(request, product_id):
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/artwork_details.html', context)
