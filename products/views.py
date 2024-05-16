de853b83b8d63f7af80cd8b752052d4940a740ae
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-05-16 21:21:39 +0000

    Search and category dropdown list linked via views and base/nav.html

--- products/views.py
+++ products/views.py
@@ -1,14 +1,36 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
 
 
 #A view to render the art_products
 def art_products(request):
 
     products = Product.objects.all()
+    search = None
+    categories = None
+
+    if request.GET:
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(art_category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            search = request.GET['q']
+            if not search:
+                messages.error(request, "No search request!")
+                return redirect(reverse ('products'))
+
+            searches = Q(artwork__icontains=search) | Q(description__icontains=search)
+            products = products.filter(searches)
 
     context = {
         'products': products,
+        'search_term': search,
+        'current_categories': categories,
     }
     return render(request, 'products/art_products.html', context)
 
