1ac56d3faec530e5b0a39c989e7913ddd2bdf0b2
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-06-04 20:52:54 +0000

    modified remove_from_cart code to fix bug displayed in console. Tested and worked ok!

--- shop_cart/views.py
+++ shop_cart/views.py
@@ -69,25 +69,24 @@ def adjust_cart(request, item_id):
     return redirect(reverse('view_cart'))
 
 
-def remove_from_cart(request, item_id):
+def remove_from_cart(request, product_id):
     """Remove the item from the shopping bag"""
 
     try:
-        product = get_object_or_404(Product, pk=item_id)
+        product = get_object_or_404(Product, pk=product_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
         cart = request.session.get('cart', {})
 
         if size:
-            del cart[item_id]['items_by_size'][size]
-            if not cart[item_id]['items_by_size']:
-                cart.pop(item_id)
+            del cart[product_id]['items_by_size'][size]
+            if not cart[product_id]['items_by_size']:
+                cart.pop(product_id)
         else:
-            cart.pop(item_id)
+            cart.pop(product_id)
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
-
     except Exception as e:
         return HttpResponse(status=500)
