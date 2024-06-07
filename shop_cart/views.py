daff29574b38863d1392a509436186d4a2981783
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-06-07 09:40:02 +0000

    toasts/messages modified and success toast tested

--- shop_cart/views.py
+++ shop_cart/views.py
@@ -13,28 +13,25 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
-    product = get_object_or_404(Product, pk=item_id)
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
-        print('SIZE: ', size)
     cart = request.session.get('cart', {})
 
     if size:
         if item_id in list(cart.keys()):
             if size in cart[item_id]['items_by_size'].keys():
                 cart[item_id]['items_by_size'][size] += quantity
+                messages.success(request, f'{product.artwork} added to shopping cart')
             else:
                 cart[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'{product.artwork} added to shopping cart')
         else:
             cart[item_id] = {'items_by_size': {size: quantity}}
-    else:
-        if item_id in list(cart.keys()):
-            cart[item_id] += quantity
-        else:
-            cart[item_id] = quantity
+            messages.success(request, f'{product.artwork} added to shopping cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
