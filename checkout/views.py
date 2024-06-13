c8517026b6b1c645fa8835479a0afdd94820efba
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-06-13 09:21:42 +0000

    checkout page modified and card element added from Stripe

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PQrw2CBk1oXddjNZbJY3OSdzDPqeyG8tbtnGPbEAycoFXthzyCop1B8aZ1NXBMBM3UA3DC5srDEERPrSETv7KnK00H3PL4kSt',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
