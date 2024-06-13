68d81b11676189892dbdf80383a3e03bfd117601
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-06-13 13:26:53 +0000

    delivery and grand total fields added to checkout models, tested delivery and grand total template output in shop cart

--- checkout/models.py
+++ checkout/models.py
@@ -20,14 +20,20 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
+    delivery_cost =  models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
     
     def _generate_order_number(self):
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
         self.order_total = self.lineitems.aggregate(Sum('item_total'))['item_total__sum']
+        
         if self.order_total:
+            self.delivery_cost = 0
+            self.grand_total = self.order_total + self.delivery_cost
             self.save()
 
     def save(self, *args, **kwargs):
--- shop_cart/contexts.py
+++ shop_cart/contexts.py
@@ -33,10 +33,17 @@ def cart_contents(request):
                     'size': size,
                 })
 
+    if total:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+
+    grand_total = delivery + total
+    
     context = {
         'cart_items': cart_items,
         'total': total,
         'product_count': product_count,
+        'delivery': delivery,
+        'grand_total': grand_total,
     }
 
     return context
