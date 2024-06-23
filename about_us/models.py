e3f00f1342527d8964c5e38d844225aebcc008a8
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-06-23 21:05:05 +0000

    webhook_handler and webhooks.py modified

--- about_us/models.py
+++ about_us/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Username(models.Model):
+
+    username = models.TextField(null = True, blank=True)
+    
+    def __str__(self):
+        return self.username
+
+
+class Review(models.Model):
+
+    review = models.TextField(null=True, blank=True)
+    review_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.review
