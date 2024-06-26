9d8a5cbecd3ee5d353cae15bd242f590fbf5470c
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-06-26 14:36:06 +0000

    review_dates displayed on about_us reviews, and UserProfile model created

--- about_us/models.py
+++ about_us/models.py
@@ -14,7 +14,7 @@ class Review(models.Model):
     username_id = models.ForeignKey('Username', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.TextField(null=True, blank=True)
     review = models.TextField(null=True, blank=True)
-    review_date = models.DateTimeField(auto_now_add=True)
+    review_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     
     def __str__(self):
         return self.review
