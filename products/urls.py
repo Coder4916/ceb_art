90f5c71d09e8abc8e2614acf2fbef03163ab695e
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-07-01 09:24:20 +0000

    update_product template and functionality added and tested

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.art_products, name='products'),
     path('<int:product_id>/', views.artwork_details, name='artwork_details'),
     path('create/', views.create_product, name='create_product'),
+    path('update/<int:product_id>/', views.update_product, name='update_product'),
 ]
