393a0601b3fec59db69adbf05017b01521805200
Craig Bailey <c.bailey84@hotmail.com>
2024-07-05 12:22:15 +0000

    project settings updated ready for deployment

--- ceb_chalk_art/settings.py
+++ ceb_chalk_art/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-coder4916-cebart-exjs31guuwj.ws.codeinstitute-ide.net',
