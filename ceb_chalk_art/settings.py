04d38e73431fb64868f2fcad29fd8860ec0e8c2c
Coder4916 Bailey <c.bailey84@hotmail.com>
2024-09-25 20:17:46 +0000

    notes added to checkout model, and heroku site added to trusted origins

--- ceb_chalk_art/settings.py
+++ ceb_chalk_art/settings.py
@@ -29,6 +29,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-coder4916-cebart-exjs31guuwj.ws.codeinstitute-ide.net',
+    '8000-coder4916-cebart-uumsw8xooj4.ws-eu116.gitpod.io',
     'ceb-art-3810bf4d9953.herokuapp.com',
     'localhost',
 ]
@@ -135,7 +136,7 @@ else:
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
-    "https://*.herokuapp.com",
+    "https://ceb-art-3810bf4d9953.herokuapp.com/",
     "https://www.gitpod.io/",
     'https://8000-coder4916-cebart-exjs31guuwj.ws.codeinstitute-ide.net'
 ]
@@ -218,7 +219,7 @@ STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'cebart@example.com'
+    DEFAULT_FROM_EMAIL = 'craigebailey916@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
